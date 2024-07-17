9434303394fbe8b483d144a6242e24fe7f9f1589
JacobGale <jacob.gale@ufl.edu>
2024-07-17 14:03:39 -0400

    Adding logout functionality and displaying user info on profile page

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,10 +1,48 @@
 import NavBar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Profile() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  const signOut = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    const userData = async () => {
+      const response = await axios.get("http://localhost:8081/users/get", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      setUsername(response.data.username);
+      setEmail(response.data.email);
+    };
+    userData();
+  });
+
   return (
     <div>
       <NavBar />
-      <div>Profile</div>
+      <div className="container mx-auto mt-8 p-4">
+        <div className="max-w-md mx-auto bg-white shadow-md rounded-lg overflow-hidden">
+          <div className="p-4">
+            <h1 className="text-2xl font-semibold mb-2">{username}</h1>
+            <p className="text-gray-600 mb-4">{email}</p>
+            <button
+              onClick={signOut}
+              className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
